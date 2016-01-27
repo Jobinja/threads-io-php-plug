0d97c7a3838b13ca91b6a64581c468bfefe17623
Reza Shadman <pcfeeler@gmail.com>
2016-01-27 12:51:46 +0330

    Make client work with psr compatible guzzle version, catch and transform exceptions and some refactor

--- src/ThreadsIoClient.php
+++ src/ThreadsIoClient.php
@@ -1,20 +1,31 @@
 <?php
 
-namespace Wabel\ThreadsIo;
+namespace Jobinja\ThreadsIo;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Message\RequestInterface;
-use Wabel\ThreadsIo\Exceptions\ThreadsIoPlugException;
-use \Wabel\ThreadsIo\Response\Response;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\RequestOptions;
+use Jobinja\ThreadsIo\Exceptions\ThreadsIoBadRequestException;
+use Jobinja\ThreadsIo\Exceptions\ThreadsIoInvalidKeyException;
+use Jobinja\ThreadsIo\Exceptions\ThreadsIoPlugException;
+use Jobinja\ThreadsIo\Exceptions\ThreadsIoServerException;
+use Jobinja\ThreadsIo\Response\Response;
 
 /**
  * This is an abstraction layer between the Service and the HTTP Requests
  * It implements the basic methods of the Threads.io API
  *
  * Class ThreadsIoClient
- * @package Wabel\ThreadsIo
+ *
+ * @package Jobinja\ThreadsIo
  */
-class ThreadsIoClient {
+class ThreadsIoClient
+{
+    /**
+     * Threads endpoint url
+     */
+    const END_POINT = 'https://input.threads.io/v1/';
 
     /**
      * Keyword action for user identification
@@ -38,50 +49,55 @@ class ThreadsIoClient {
 
     /**
      * The Guzzle client (v5) used to make HTTP Requests
+     *
      * @var Client
      */
     private $httpClient;
 
     /**
      * The API Key provided by Threads.io to connect to your account
+     *
      * @var string
      */
     private $eventKey;
 
     /**
      * Client constructor
+     *
      * @param string $eventKey
+     * @param null   $endPoint
      */
-    public function __construct($eventKey) {
+    public function __construct($eventKey, $endPoint = null)
+    {
         $this->setEventKey($eventKey);
 
         // Setting up base API URL and Basic Authentication
         $this->setHttpClient(new Client([
-            'base_url' => 'https://input.threads.io/v1/',
-            'defaults' => ['auth' => [$this->getEventKey(), ""]]
+            'base_uri' => $endPoint ?: static::END_POINT,
+            RequestOptions::AUTH => [$this->getEventKey(), ""],
         ]));
     }
 
     /**
      * Basic implementation of the API method "identify"
      *
-     * @param string $userId
+     * @param string             $userId
      * @param \DateTimeImmutable $datetime
-     * @param array $traits
+     * @param array              $traits
      *
      * @return Response
      * @throws ThreadsIoPlugException
      */
-    public function identify($userId, \DateTimeImmutable $datetime, $traits) {
-
-        if(!is_array($traits)){
+    public function identify($userId, \DateTimeImmutable $datetime, $traits)
+    {
+        if (!is_array($traits)) {
             throw new ThreadsIoPlugException("The traits you passed to the user are wrong. Please verify its format (array) or values.");
         }
 
         $params = [
-            "userId" => $userId,
+            "userId"    => $userId,
             "timestamp" => $this->formatDate($datetime),
-            "traits" => $traits
+            "traits"    => $traits,
         ];
         $request = $this->createRequest(self::IDENTIFY_ACTION, $params);
         return $this->call($request);
@@ -90,25 +106,26 @@ class ThreadsIoClient {
     /**
      * Basic implementation of the API method "track"
      *
-     * @param string $userId
-     * @param string $event
+     * @param string             $userId
+     * @param string             $event
      * @param \DateTimeImmutable $datetime
-     * @param array $properties
+     * @param array              $properties
      *
      * @return Response
      * @throws ThreadsIoPlugException
      */
-    public function track($userId, $event,\DateTimeImmutable $datetime, $properties) {
+    public function track($userId, $event, \DateTimeImmutable $datetime, $properties)
+    {
 
-        if(!is_array($properties)){
+        if (!is_array($properties)) {
             throw new ThreadsIoPlugException("The properties you passed to the tracking function are wrong. Please verify its format (array) or values.");
         }
 
         $request = $this->createRequest(self::TRACK_ACTION, [
-            "userId" => $userId,
-            "event" => $event,
-            "timestamp" => $this->formatDate($datetime),
-            "properties" => $properties
+            "userId"     => $userId,
+            "event"      => $event,
+            "timestamp"  => $this->formatDate($datetime),
+            "properties" => $properties,
         ]);
         return $this->call($request);
     }
@@ -116,26 +133,27 @@ class ThreadsIoClient {
     /**
      * Basic implementation of the API method "page"
      *
-     * @param string $userId
-     * @param string $name
-     * @param array $properties
+     * @param string             $userId
+     * @param string             $name
+     * @param array              $properties
      * @param \DateTimeImmutable $datetime
      *
      * @return Response
      * @throws ThreadsIoPlugException
      */
-    public function page($userId, $name, $properties,\DateTimeImmutable $datetime) {
+    public function page($userId, $name, $properties, \DateTimeImmutable $datetime)
+    {
 
-        if(!is_array($properties)){
+        if (!is_array($properties)) {
             throw new ThreadsIoPlugException("The properties you passed to the page function are wrong. Please verify its format (array) or values.");
         }
 
         $request = $this->createRequest(self::VISIT_ACTION, [
-            "eventKey" => $this->getEventKey(),
-            "userId" => $userId,
-            "name" => $name,
+            "eventKey"   => $this->getEventKey(),
+            "userId"     => $userId,
+            "name"       => $name,
             "properties" => $properties,
-            "timestamp" => $this->formatDate($datetime)
+            "timestamp"  => $this->formatDate($datetime),
         ]);
         return $this->call($request);
     }
@@ -143,15 +161,16 @@ class ThreadsIoClient {
     /**
      * Basic implementation of the API method "remove"
      *
-     * @param $userId
+     * @param                    $userId
      * @param \DateTimeImmutable $datetime
      *
      * @return Response
      */
-    public function remove($userId,\DateTimeImmutable $datetime) {
+    public function remove($userId, \DateTimeImmutable $datetime)
+    {
         $request = $this->createRequest(self::REMOVE_ACTION, [
             "timestamp" => $this->formatDate($datetime),
-            "userId" => $userId
+            "userId"    => $userId,
         ]);
         return $this->call($request);
     }
@@ -159,40 +178,60 @@ class ThreadsIoClient {
     /**
      * Executes the HTTP Request given in argument
      *
-     * @param RequestInterface $request
+     * @param array $request
      *
      * @return Response
      */
-    private function call($request) {
-        $response = $this->getHttpClient()->send($request);
-        return new Response($response->getBody());
+    private function call($request)
+    {
+        try {
+            $response = $this->getHttpClient()->request($request['method'], $request['action'], $request['params']);
+            return new Response($response->getBody());
+        } catch (ClientException $e) {
+            if ($e->getResponse()->getStatusCode() == 401) {
+                throw new ThreadsIoInvalidKeyException($e);
+            }
+            throw new ThreadsIoBadRequestException($e);
+        } catch (ServerException $e) {
+            throw new ThreadsIoServerException($e);
+        }
     }
 
     /**
      * Format the date based on the ISO 8601 date formats
+     *
      * @see https://docs.threads.io/docs/threads-timestamp-format
      *
      * @param \DateTimeImmutable $datetime
      *
      * @return string
      */
-    private function formatDate(\DateTimeImmutable $datetime) {
-        return  str_replace('+00:00', '.000Z', $datetime->setTimezone(new \DateTimeZone('UTC'))->format('c'));
+    private function formatDate(\DateTimeImmutable $datetime)
+    {
+        return str_replace('+00:00', '.000Z', $datetime->setTimezone(new \DateTimeZone('UTC'))->format('c'));
     }
 
     /**
      * Prepare the Request to be executed by $this->call() method
      *
-     * @param $action
+     * @param        $action
      *
-     * @return \GuzzleHttp\Message\RequestInterface
+     * @param array  $params
+     * @param string $method
+     * @return array
      */
-    private function createRequest($action, $params) {
-        return $this->httpClient->createRequest("POST", $action, ["json" => $params]);
+    private function createRequest($action, array $params = [], $method = 'POST')
+    {
+        return [
+            'method' => $method,
+            'action' => $action,
+            'params' => ['json' => $params]
+        ];
     }
 
     /**
      * Getter for $httpClient
+     *
      * @return Client
      */
     public function getHttpClient()
@@ -202,6 +241,7 @@ class ThreadsIoClient {
 
     /**
      * Setter for $httpClient
+     *
      * @param Client $httpClient
      */
     public function setHttpClient($httpClient)
@@ -211,6 +251,7 @@ class ThreadsIoClient {
 
     /**
      * Getter for $eventKey
+     *
      * @return string
      */
     public function getEventKey()
@@ -220,6 +261,7 @@ class ThreadsIoClient {
 
     /**
      * Setter for $eventKey
+     *
      * @param string $eventKey
      */
     public function setEventKey($eventKey)
