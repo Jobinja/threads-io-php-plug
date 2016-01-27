0e4467d76cec36fb2206ddeb5c420dececc33806
Reza Shadman <pcfeeler@gmail.com>
2016-01-27 12:53:01 +0330

    Add exception for catching server errors

--- src/Exceptions/ThreadsIoServerException.php
+++ src/Exceptions/ThreadsIoServerException.php
@@ -2,7 +2,18 @@
 
 namespace Jobinja\ThreadsIo\Exceptions;
 
-class ThreadsIoServerException
-{
+use GuzzleHttp\Exception\ServerException;
 
+class ThreadsIoServerException extends ServerException implements ThreadsIoExceptionInterface
+{
+    public function __construct(ServerException $e)
+    {
+        parent::__construct(
+            $e->getMessage(),
+            $e->getRequest(),
+            $e->getResponse(),
+            $e,
+            $e->getHandlerContext()
+        );
+    }
 }
